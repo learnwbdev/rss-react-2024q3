dc9b341103ee8ea290a0cdb6c38c0d837d8e7e7b
Alexandra Paramonova <alpar.wb@gmail.com>
2024-07-08 03:48:26 +0500

    feat: add input component

---
+++ src/shared/ui/input/index.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import styles from './input.module.css';
+
+interface InputProps {
+  name: string;
+  label: string;
+}
+
+export class Input extends Component<InputProps> {
+  render(): React.ReactNode {
+    return (
+      <label className={styles.label}>
+        <span className={styles.label_text}>{`${this.props.label}:`}</span>
+        <input type="text" name={this.props.name} className={styles.input} />
+      </label>
+    );
+  }
+}
