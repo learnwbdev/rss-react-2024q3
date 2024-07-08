20be0f3a5a4a20349a8f4ed5731fe898f7eba77d
Alexandra Paramonova <alpar.wb@gmail.com>
2024-07-08 11:01:13 +0500

    feat: wrap app with ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from '@app';
+import { ErrorBoundary } from '@shared/utils';
+import { ErrorFallback } from '@shared/ui';
 import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<ErrorFallback />}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
