9d4272834cd274a85a0b90bde861d830785af1aa
Alexandra Paramonova <alpar.wb@gmail.com>
2024-07-14 08:24:31 +0500

    chore: configure project to import svg files as react components

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    svgr({ svgrOptions: { exportType: 'default', ref: true, svgo: false, titleProp: true }, include: '**/*.svg' }),
+    dts(),
+    react(),
+    tsconfigPaths(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
