30bf1ee1923878fa0355c45a04021af4b50a67f4
Alexandra Paramonova <alpar.wb@gmail.com>
2024-07-14 09:30:35 +0500

    feat: add color prop for Button component, add css color variable for warning

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -5,11 +5,14 @@ interface ButtonProps {
   text: string;
   onClick: React.MouseEventHandler;
   disabled?: boolean;
+  color?: 'primary' | 'warning';
 }
 
-export const Button = ({ text, onClick, disabled = false }: ButtonProps): ReactNode => {
+export const Button = ({ text, onClick, disabled = false, color = 'primary' }: ButtonProps): ReactNode => {
+  const colorClass = color !== 'primary' ? styles[`btn__${color}`] : '';
+
   return (
-    <button type="button" className={styles.btn} onClick={onClick} disabled={disabled}>
+    <button type="button" className={`${styles.btn} ${colorClass}`} onClick={onClick} disabled={disabled}>
       {text}
     </button>
   );
