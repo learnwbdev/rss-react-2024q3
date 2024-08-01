fbe8fcaf5272efc28ade04ca838802d748e6f4ce
Alexandra Paramonova <alpar.wb@gmail.com>
2024-08-01 17:53:21 +0500

    fix: correct useGetPersonByIdQueries to use createSelector to correct redux memo warning

--- src/features/download-items/hooks/index.ts
+++ src/features/download-items/hooks/index.ts
@@ -1,14 +1,25 @@
 import { useEffect } from 'react';
-import { SerializedError } from '@reduxjs/toolkit';
+import { createSelector, SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import { Person } from '@shared/api';
-import { peopleApi, useAppDispatch, useAppSelector } from '@shared/store';
+import { peopleApi, useAppDispatch, useAppSelector, RootState } from '@shared/store';
 
 interface UseGetPersonByIdQueriesResult {
   data?: Person[];
   error?: FetchBaseQueryError | SerializedError;
   isLoading: boolean;
 }
+
+const selectPersonById = (state: RootState, id: string) => {
+  return peopleApi.endpoints.getPersonById.select(id)(state);
+};
+
+const getSelectCombinedResults = () =>
+  createSelector(
+    (state: RootState): RootState => state,
+    (_, ids: string[]): string[] => ids,
+    (state: RootState, ids: string[]) => ids.map((id) => selectPersonById(state, id))
+  );
 
 export const useGetPersonByIdQueries = (ids: string[]): UseGetPersonByIdQueriesResult => {
   const dispatch = useAppDispatch();
@@ -24,11 +35,9 @@ export const useGetPersonByIdQueries = (ids: string[]): UseGetPersonByIdQueriesR
     };
   }, [dispatch, ids]);
 
-  const combinedResults = useAppSelector((state) => {
-    return ids.map((id) => {
-      return peopleApi.endpoints.getPersonById.select(id)(state);
-    });
-  });
+  const selectCombinedResults = getSelectCombinedResults();
+
+  const combinedResults = useAppSelector((state) => selectCombinedResults(state, ids));
 
   const isLoading = combinedResults.some((result) => result.isLoading);
 
