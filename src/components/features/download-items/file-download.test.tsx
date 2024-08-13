6cc588c9586418adbc28c423bb79fa0c6b6ac7e7
Alexandra Paramonova <alpar.wb@gmail.com>
2024-08-13 15:49:56 +0500

    test: add tests for FileDownload

---
+++ src/components/features/download-items/file-download.test.tsx
@@ -0,0 +1,59 @@
+import { render, waitFor } from '@tests/utils';
+import { describe, it, expect, vi } from 'vitest';
+import mockRouter from 'next-router-mock';
+import { FileDownload } from './file-download';
+
+vi.mock('next/router', async () => {
+  const routerMock = await vi.importActual('next-router-mock');
+  return routerMock;
+});
+
+describe('FileDownload', () => {
+  it('renders and triggers download only once', async () => {
+    const mockUrl = 'blob:http://example.com/download';
+    const mockFilename = 'test-file.csv';
+    const mockOnDownloadComplete = vi.fn();
+
+    const clickSpy = vi.spyOn(HTMLAnchorElement.prototype, 'click').mockImplementation(() => ({}));
+
+    const { getByText } = render(
+      <FileDownload url={mockUrl} filename={mockFilename} onDownloadComplete={mockOnDownloadComplete} />
+    );
+
+    const linkElement = getByText('Download');
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toBeInstanceOf(HTMLAnchorElement);
+
+    if (linkElement instanceof HTMLAnchorElement) {
+      expect(linkElement.href).toBe(mockUrl);
+      expect(linkElement.download).toBe(mockFilename);
+    } else {
+      throw new Error('Expected element to be an HTMLAnchorElement');
+    }
+
+    linkElement.click();
+
+    await waitFor(() => {
+      expect(mockOnDownloadComplete).toHaveBeenCalledTimes(1);
+    });
+
+    clickSpy.mockRestore();
+  });
+
+  it('does not call onDownloadComplete if it is undefined', async () => {
+    const mockUrl = 'blob:http://example.com/download';
+    const mockFilename = 'test-file.csv';
+
+    const clickSpy = vi.spyOn(HTMLAnchorElement.prototype, 'click').mockImplementation(() => ({}));
+
+    const initialPath = `/?page=1`;
+
+    await mockRouter.push(initialPath);
+
+    render(<FileDownload url={mockUrl} filename={mockFilename} />);
+
+    expect(clickSpy).toHaveBeenCalled();
+
+    clickSpy.mockRestore();
+  });
+});
